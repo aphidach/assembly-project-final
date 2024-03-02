222c9c6bfbdf952d76274eaa6cbf0b403f6bc7a5
PumPuiEvo <piyungkoon.s@kkumail.com>
2024-03-02 23:34:50 +0700

    div with show in console finish

--- div_with4rem.s
+++ div_with4rem.s
@@ -4,6 +4,11 @@ section .data
     div_ANS dq 0
     div_rem dq 0
 
+    msg db "Num "
+    len_msg equ ($-msg)
+    dot db "."
+    const10 dq 10
+
 section .text
 _start:
     mov rdx, 0
@@ -27,19 +32,60 @@ div_result:
         ;rax req answer for show ANS
     mov qword[div_ANS], rax
 
-    mov rax, rdx ;for find rem by div
+    mov rax, rdx    ;for find rem by div
     imul rax, 10000 ;for calculate 4 point floting
-    mov rdx, 0 ; clear rdx for div rax only
-    div rbx ;rax = rem 4 point
+    mov rdx, 0      ; clear rdx for div rax only
+    div rbx         ;rax = rem 4 point
 
     mov qword[div_rem], rax
     ;Result  qword[div_ANS].qword[div_rem]
 
     ;mov 1's arugment, qword[div_ANS]
     ;mov 2's arugment, qword[div_rem]
-    ;call showresutl ;call subroutine to display result 
+    call showresult ;call subroutine to display result 
 endmodule:
     pop rbx
     pop rdx
+    pop rax
+    ret
+
+
+showresult:
+    mov rax, qword[div_ANS]
+    mov rdx, 0
+    call printNumber
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, dot
+    mov rdx, 1
+    syscall
+
+    mov rax, qword[div_rem]
+    mov rdx, 0
+    call printNumber    
+    ret
+printNumber:
+    push rax
+    push rdx
+    xor rdx, rdx        ;rdx:rax = number
+    div qword[const10]  ;rax = quotient, rdx = remainder
+    test rax, rax       ;Is quotient zero?
+    je .l1              ;yes, don't display it
+    call printNumber    ;Display the quotient
+.l1:
+    lea rax, [rdx+'0']
+    call printCharacter  ;Display the remainder
+    pop rdx
+    pop rax
+    ret
+
+printCharacter:
+    push rax
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, rsp
+    mov rdx, 1
+    syscall
     pop rax
     ret
