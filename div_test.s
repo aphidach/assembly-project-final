5c8c282e2d5110254ed49e96041427410f06f497
PumPuiEvo <piyungkoon.s@kkumail.com>
2024-03-02 19:27:34 +0700

    convert int to string

--- div_test.s
+++ div_test.s
@@ -4,6 +4,12 @@ section .data
     msg db "Hello world!",10
     len_msg equ ($-msg)
 
+    err_msg db "Error",10
+    len_err_msg equ ($-err_msg)
+
+    div_ANS dq 0
+    div_rem dq 0
+
 section .text
 _start:
     mov rax,1
@@ -26,8 +32,64 @@ div_result:
     push rax
     push rdx
 
+    mov qword[div_ANS], rax
+    mov qword[div_rem], rdx
+
+    ;on debug -----------------------------VVVVVV---------------------------------
+    mov rax,1
+    mov rdi,1
+    mov rsi, err_msg ;should convert to string first :feture on going
+    mov rdx,len_err_msg
+    syscall
+
+    mov rax,1
+    mov rdi,1
+    mov rsi, err_msg ;should convert to string first :feture on going
+    mov rdx,len_err_msg
+    syscall
+    ;on debug -----------------------------^^^^^^---------------------------------
 
 endmodule:
     pop rdx
     pop rax
     ret
+
+
+;convert 0-9 to string
+convert:
+    push rax
+    push rsi
+    push rdi
+
+    movzx rsi, byte [rdi]   ; Get the current character
+    test rsi, rsi           ; Check for \0
+    je done
+    
+    cmp rsi, 48             ; Anything less than 0 is invalid
+    jl error
+    
+    cmp rsi, 57             ; Anything greater than 9 is invalid
+    jg error
+     
+    sub rsi, 48             ; Convert from ASCII to decimal 
+    imul rax, 10            ; Multiply total by 10
+    add rax, rsi            ; Add current digit to total
+    
+    inc rdi                 ; Get the address of the next character
+    jmp convert
+
+error:
+    ; console on error
+    mov rax,1
+    mov rdi,1
+    mov rsi,err_msg
+    mov rdx,len_err_msg
+    syscall            
+ 
+done:
+    pop rdi
+    pop rsi
+    pop rax
+    ret  
+
+
