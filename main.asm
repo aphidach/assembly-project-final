7429a164a726dcacb4cd5c289a6680869fbb85f1
aphidahc puratchaka <aphidach.p@kkumail.com>
2024-03-02 14:54:24 +0700

    feat: add file main.asm for start project

---
+++ main.asm
@@ -0,0 +1,17 @@
+global _start
+
+section .data
+    msg db "Hello world!",10
+    len_msg equ ($-msg)
+
+section .text
+_start:
+    mov rax,1
+    mov rdi,1
+    mov rsi,msg
+    mov rdx,len_msg
+    syscall
+
+    mov rax,60
+    mov rdi,0
+    syscall
