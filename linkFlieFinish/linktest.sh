f4aa7dad4146c7aef7b644afaf090da347690990
PumPuiEvo <piyungkoon.s@kkumail.com>
2024-03-03 18:17:35 +0700

    change filename

--- linkFlieFinish/linktest.sh
+++ linkFlieFinish/linktest.sh
@@ -1,4 +1,4 @@
-yasm -f elf64 -g dwarf2 -o div_4.o div_4.s
+yasm -f elf64 -g dwarf2 -o main.o main.s
 yasm -f elf64 -g dwarf2 -o printNumber.o printNumber.s
-ld -o div_new div_4.o printNumber.o
+ld -o div_new main.o printNumber.o
 ./div_new
