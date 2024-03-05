42a259746140caa73823cc59c2fcc7cd08d7d9af
PumPuiEvo <piyungkoon.s@kkumail.com>
2024-03-05 23:09:13 +0700

    feat: add debug mode

--- compileDebug.sh
+++ compileDebug.sh
@@ -1,11 +1,11 @@
 echo yasm req_input
-yasm -f elf64 -o req_input.o req_input.s
+yasm -f elf64 -g dwarf2 -o req_input.o req_input.s
 
 echo yasm printNumber
-yasm -f elf64 -o printNumber.o printNumber.s
+yasm -f elf64 -g dwarf2 -o printNumber.o printNumber.s
 
 echo yasm main
-yasm -f elf64 -o main.o main.s
+yasm -f elf64 -g dwarf2 -o main.o main.s
 
 echo linkfile
 ld -o div_new main.o printNumber.o req_input.o
