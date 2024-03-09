5003c885bb261ebe82d195440f545cac10c40dda
PumPuiEvo <piyungkoon.s@kkuamil.com>
2024-03-10 00:33:05 +0700

    change strc

--- controlFile/div_with4rem.s
+++ controlFile/div_with4rem.s
@@ -88,34 +88,6 @@ showresult:
     syscall 
     ret
 
-printNumber:
-    push rax
-    push rdx
-    xor rdx, rdx        ;clear rdx
-    div qword[const10]  ;rax = quotient, rdx = remainder
-    test rax, rax       ;== quotient zero?
-    je .char_convert    
-    call printNumber    ;Display the quotient
-.char_convert  :
-    lea rax, [rdx + '0']      ;rax = rdx + '0' //number to ascii
-    call print_character    ;Display the remainder
-    pop rdx
-    pop rax
-    ret
-
-print_character:
-    push rax            ;push the Character in stack
-
-    mov rax, SYS_write
-    mov rdi, STDOUT
-    mov rsi, rsp        ;rsi = address of Character 
-    mov rdx, 1
-    syscall
-
-    pop rax 
-    ret
-
-
 zero_precheck:
     push rax
     push rbx
@@ -149,4 +121,31 @@ zero_precheck:
     pop rdx
     pop rbx
     pop rax
+    ret
+
+printNumber:
+    push rax
+    push rdx
+    xor rdx, rdx        ;clear rdx
+    div qword[const10]  ;rax = quotient, rdx = remainder
+    test rax, rax       ;== quotient zero?
+    je .char_convert    
+    call printNumber    ;Display the quotient
+.char_convert  :
+    lea rax, [rdx + '0']      ;rax = rdx + '0' //number to ascii
+    call print_character    ;Display the remainder
+    pop rdx
+    pop rax
+    ret
+
+print_character:
+    push rax            ;push the Character in stack
+
+    mov rax, SYS_write
+    mov rdi, STDOUT
+    mov rsi, rsp        ;rsi = address of Character 
+    mov rdx, 1
+    syscall
+
+    pop rax 
     ret
